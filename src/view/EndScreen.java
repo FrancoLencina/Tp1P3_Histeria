aa8125355d0639b96d4403b3709444e63c6fe5a4
Matías Duarte <duarte.lmd28@gmail.com>
2025-05-21 22:57:12 -0300

    Ajustado el tamaño de la fuente en la endScreen

--- src/view/EndScreen.java
+++ src/view/EndScreen.java
@@ -1,17 +1,25 @@
 package view;
 
 import javax.swing.JFrame;
-import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
-import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+
 import Scores.ScoreFileEditor;
 import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class EndScreen {
 
 	JFrame frame;
 	private int score;
 	private int difficulty;
+	private JLabel label;
 
 	/**
 	 * Create the application.
@@ -32,25 +40,48 @@ public class EndScreen {
 		frame.setExtendedState(frame.MAXIMIZED_BOTH);
 	    frame.setBounds(100, 100, 720, 480);
 		frame.getContentPane().setBackground(new Color(50, 50, 50));
-		frame.getContentPane().setLayout(new CardLayout(350, 300));
+		//frame.getContentPane().setLayout(new BorderLayout());
 
-		JTextArea txt = new JTextArea();
-		txt.setFont(new Font("Arial Black", Font.PLAIN, 12));
-		txt.setForeground(new Color(255, 255, 255));
-		txt.setBackground(new Color(50, 50, 50));
+		JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBackground(new Color(50, 50, 50));
+        label = new JLabel();
+        label.setForeground(Color.WHITE);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
 
 		ScoreFileEditor sfe = new ScoreFileEditor("src/Scores/scores.txt", score);
 
 		if (sfe.readScore(difficulty) < score) {
-			txt.setText("      ¡Felicitaciones!" + "\nGanaste en " + score + " turnos!"
-					+ "\nLa mejor puntuación fue de: " + sfe.readScore(difficulty));
+			label.setText("      ¡Felicitaciones!  " + "  \nGanaste en " + score + " turnos!"
+					+ "  \nLa mejor puntuación fue de: " + sfe.readScore(difficulty));
 		} else {
 			sfe.replaceScore(difficulty);
-			txt.setText("    ¡Felicitaciones!" + "\nGanaste en " + score + " turnos!"
-					+ "\n¡Nueva puntuación récord!");
+			label.setText("    ¡Felicitaciones!  " + "  \nGanaste en " + score + " turnos!"
+					+ "  \n¡Nueva puntuación récord!");
 		}
+		
+		label.setFont(new Font("Arial Black", Font.PLAIN, 24));
 
-		frame.getContentPane().add(txt);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		panel.add(label);
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+
+	
+	
+	  frame.addComponentListener(new ComponentAdapter() {
+          public void componentResized(ComponentEvent e) {
+              changeFontSize();
+          }
+      });
+
+      frame.setVisible(true);
+      changeFontSize(); 
+  }
+
+	 private void changeFontSize() {
+	        int width = frame.getWidth();
+	        int newFontSize = Math.max(18, width / 40); 
+
+	        label.setFont(new Font("Arial Black", Font.PLAIN, newFontSize));
+	    }
+
 }
